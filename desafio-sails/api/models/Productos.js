93dcbbd316d0bde1200329a73aaa5279a12f46f4
Damian <damianabalos@hotmail.com>
2022-07-12 12:27:06 -0300

    first commit

---
+++ desafio-sails/api/models/Productos.js
@@ -0,0 +1,16 @@
+/**
+ * Productos.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+  attributes: {
+    nombre: { type: 'string', required: true },
+    precio: { type: 'number', required: true },
+    stock: { type: 'number', required: true },
+  },
+
+};
+
